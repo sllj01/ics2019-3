691379c7208085aeb767f48b609bdd343bf72dde
tracer-ics2019 <tracer@njuics.org>
2019-09-14 12:58:56 +0800

    >  compile 181220014 derek Linux eureka 5.0.0-27-generic #28~18.04.1-Ubuntu SMP Thu Aug 22 03:00:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  12:58:56 up 28 min,  1 user,  load average: 0.16, 0.59, 0.61 77eceb70a985b45b9d32b916e74c02b94acf4b

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -1,7 +1,7 @@
 #include "nemu.h"
 #include <stdlib.h>
 #include <time.h>
-
+#include <inttypes.h>
 const char *regsl[] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
 const char *regsw[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
 const char *regsb[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
