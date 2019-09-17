ff0cfb4c129b397898a269c0b9a86932c6b1db09
tracer-ics2019 <tracer@njuics.org>
2019-09-17 19:56:22 +0800

    >  compile 181220014 derek Linux eureka 5.0.0-27-generic #28~18.04.1-Ubuntu SMP Thu Aug 22 03:00:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:56:22 up  1:54,  1 user,  load average: 0.52, 0.44, 0.43 357facc0531a184820bd7b82531b96c4bd664c0

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -80,6 +80,7 @@ static int cmd_x(char *args){
 static int cmd_p(char *args){
 	char *arg;
 	strtok_r(args, " ", &arg);
+	printf("%s", arg);
 	bool success=true;
 	uint32_t num = expr(arg, &success);
 	printf("%u", num);
