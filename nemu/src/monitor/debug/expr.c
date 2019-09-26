0832eb8c6ed897f2ce773aa9fdfc9a658f6e00a3
tracer-ics2019 <tracer@njuics.org>
2019-09-26 22:03:51 +0800

    >  compile 181220014 derek Linux eureka 5.0.0-29-generic #31~18.04.1-Ubuntu SMP Thu Sep 12 18:29:21 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:03:51 up  1:13,  1 user,  load average: 0.12, 0.29, 0.33 b05d45751589d87d24ffcd39ffc91d055029a0e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -33,7 +33,7 @@ static struct rule {
   {"==", TK_EQ},         // equal
   {"\\(", TK_LCOM},		// left combination mark
   {"\\)", TK_RCOM},		// right combination mark
-  {"0x[\\dABCDEF]+", TK_HEXADECIMAL},
+  {"0x[0-9ABCDEF]+", TK_HEXADECIMAL},
   {"[1-9]+", TK_NUM10},  // number in 10 jinzhi
   {"\\$[a-zA-Z]{2,3}", TK_REGNAME},
   {"!=", TK_NOTEQUAL},
