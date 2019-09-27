bb0930d7c6fcd1f7fe5557a1454c1f7b21563ce5
tracer-ics2019 <tracer@njuics.org>
2019-09-27 16:08:30 +0800

    >  compile 181220014 derek Linux eureka 5.0.0-29-generic #31~18.04.1-Ubuntu SMP Thu Sep 12 18:29:21 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:08:30 up  1:06,  1 user,  load average: 0.25, 0.56, 0.57 c277b4bfe857eb8fb9ff27bceda5aeb28cfc1253

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -19,5 +19,49 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp() {
+	if (free_==NULL) assert(0);
+	else if (head==NULL) {
+		WP* temp = free_;
+		free_ = free_->next;
+		head = temp;
+		head->next=NULL;
+		return temp;
+	}
+	else {
+		WP* temp1 = free_;
+		free_ = free_->next;
+		WP* temp2 = head;
+		while (temp2->next!=NULL) {
+			temp2 = temp2->next;
+		}
+		temp2->next = temp1;
+		temp2->next->next = NULL;
+		return temp1;
+	}
+}
 
+
+void free_wp(WP *wp) {
+	if (wp==NULL) assert(0);
+	if (head==NULL) assert(0);
+	else if (wp==head) {
+		WP* temp1 = head;
+		head = head->next;
+		if (free_==NULL) {free_=temp1; free_->next=NULL;}
+		else {
+			WP* temp2 = free_;
+			free_ = temp1;
+			free_->next = temp2;
+		}
+	}
+	else {
+		WP* temp1 = head;
+		while (head->next!=wp) temp1 = temp1->next;
+		WP* temp2 = free_;
+		free_ = temp1->next;
+		free_->next = temp2;
+		temp1->next = temp1->next->next;
+	}
+}
 
