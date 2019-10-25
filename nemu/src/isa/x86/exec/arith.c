3049ac35138e3facf08ffbb25baacd0fa3256e4e
tracer-ics2019 <tracer@njuics.org>
2019-10-26 00:16:39 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:16:39 up  7:52,  1 user,  load average: 0.39, 0.45, 0.45 c9f7de0c763a36bcec20b09af2982fde81ea5837

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -11,6 +11,23 @@ make_EHelper(sub) {
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &s0);
 
+
+  if (id_dest->width != 4) {
+    rtl_andi(&s1, &s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
+  }
+
+  rtl_update_ZFSF(&s1, id_dest->width);
+
+  // update CF
+  rtl_is_sub_carry(&s1, &s1, &s0);
+  rtl_is_sub_carry(&s0, &s0, &id_dest->val);
+  rtl_or(&s0, &s0, &s1);
+  rtl_set_CF(&s0);
+
+  // update OF
+  rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_set_OF(&s0);
+
   print_asm_template2(sub);
 }
 
