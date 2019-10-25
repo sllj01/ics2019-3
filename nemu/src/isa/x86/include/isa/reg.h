32a4f658e5cbdc455fb9716e4e77ac3513d1cb84
tracer-ics2019 <tracer@njuics.org>
2019-10-26 02:31:17 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  02:31:17 up 10:06,  1 user,  load average: 0.63, 0.53, 0.50 f35b29391dcfb3f4944c5c593df20ba06b615f4e

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -22,8 +22,20 @@ typedef struct {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
-  } gpr[8];
-  struct{rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};};
+  } gpr[9];
+  struct{rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+          struct Eflags{unsigned CF:1;
+                  unsigned not_concerned1:1;
+                  unsigned PF:1;
+                  unsigned not_concerned2:1;
+                  unsigned AF:1;
+                  unsigned not_concerned3:1;
+                  unsigned ZF:1;
+                  unsigned SF:1;
+                  unsigned not_concerned4:3;
+                  unsigned OF:1;
+                  unsigned not_concerned:20;} eflags;
+  };};
 /*
 	union {
 	uint32_t _32;
