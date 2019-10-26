4902bcc2ad6f81b7a71fb900de18f17762cc1b2e
tracer-ics2019 <tracer@njuics.org>
2019-10-26 18:08:19 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:08:19 up 30 min,  1 user,  load average: 2.54, 1.95, 1.41 e51d3b72bc825fb5c6362b901f4e2246ba30190c

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -83,17 +83,17 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   //TODO();
   if (*(result)==0)
-    cpu.eflags.ZF=0;
-  else
     cpu.eflags.ZF=1;
-  printf("ZF_updated!  result is %u, flag is %u", *result, cpu.eflags.ZF);
+  else
+    cpu.eflags.ZF=0;
+  printf("ZF_updated!  result is %u, flag is %u\n", *result, cpu.eflags.ZF);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   //TODO();
   cpu.eflags.SF = (*result>>(width*8-1))&0b1;
-  printf("SF_updated!  result is %u, flag is %u", *result, cpu.eflags.SF);
+  printf("SF_updated!  result is %u, flag is %u\n", *result, cpu.eflags.SF);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
