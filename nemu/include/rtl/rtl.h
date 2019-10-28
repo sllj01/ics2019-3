e992ec150775c702eafdac095e967e17c71deb58
tracer-ics2019 <tracer@njuics.org>
2019-10-28 23:23:23 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:23:23 up  7:11,  1 user,  load average: 0.56, 0.65, 0.58 d53243bb33788f8d474a553bafd222e2af6c0e9a

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -138,6 +138,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
+  printf("------------------width is %d", width);
   if (width==1)
   {
     t0 = *src1>>(width*8-1)&0b1;
