f49e48abdd161343bc01b875e13eab802ec0043f
tracer-ics2019 <tracer@njuics.org>
2019-10-28 19:02:51 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:02:51 up  2:50,  1 user,  load average: 0.87, 0.59, 0.43 92ea6f3dba996d68c919b0c39727439a549a4e6c

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,18 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  
+  if (pc != ref_r->pc) return false;
+  for (int index=0; index < 8; index++) {
+    if (cpu.gpr[index]._32 != ref_r->gpr[index]._32) return false;
+  }
+  if (cpu.eflags.CF!=ref_r->eflags.CF) return false;
+  if (cpu.eflags.PF!=ref_r->eflags.PF) return false;
+  if (cpu.eflags.AF!=ref_r->eflags.AF) return false;
+  if (cpu.eflags.ZF!=ref_r->eflags.ZF) return false;
+  if (cpu.eflags.SF!=ref_r->eflags.SF) return false;
+  if (cpu.eflags.OF!=ref_r->eflags.OF) return false;
+  return true;
 }
 
 void isa_difftest_attach(void) {
