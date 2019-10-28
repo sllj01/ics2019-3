562f9ef881ffa27f150323ddff5f21512039238f
tracer-ics2019 <tracer@njuics.org>
2019-10-28 08:56:50 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:56:50 up  9:39,  1 user,  load average: 0.72, 0.58, 0.49 7e35e78ecb7ea328f0f7d0921ccf4690aa630ada

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -8,26 +8,23 @@ make_EHelper(add) {
 
 make_EHelper(sub) {
   //TODO();
-  rtl_lm(&s1, &id_dest->val, id_dest->width);
+  // s0 = dest - src
   rtl_sub(&s0, &id_dest->val, &id_src->val);
+  rtl_is_sub_carry(&s1, &s0, &id_dest->val);
   operand_write(id_dest, &s0);
-
-  //TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  if (id_dest->width != 4) {
-    rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
-  }
 
   rtl_update_ZFSF(&s0, id_dest->width);
 
   // update CF
-  rtl_is_sub_carry(&s0, &s1, &id_src->val);
+
+  rtl_is_sub_carry(&s0, &s0, &s1);
   rtl_set_CF(&s0);
 
-  // // update OF
-  // rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
-  // rtl_set_OF(&s0);
+  // update OF
+  rtl_is_sub_overflow(&s1, &s0, &s1, &id_src->val, id_dest->width);
+  rtl_set_OF(&s1);
 
-  // print_asm_template2(sub);
+  print_asm_template2(sbb);
 }
 
 make_EHelper(cmp) {
--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -52,7 +52,6 @@ static inline void rtl_is_sub_carry(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1) {
   // dest <- is_carry(src1 - src2)
   //TODO();
-  if 
 }
 
 static inline void rtl_is_add_overflow(rtlreg_t* dest,
