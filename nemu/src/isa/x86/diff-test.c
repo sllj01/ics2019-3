a3bf30cdaa6d8b3513b73bb03dd9cbc5f9a350c2
tracer-ics2019 <tracer@njuics.org>
2019-10-28 21:35:47 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:35:47 up  5:23,  1 user,  load average: 0.65, 0.45, 0.44 2c680e3ab3f97ca14e60d98918274454f71ca05c

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -3,12 +3,12 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
-  // if (cpu.pc != ref_r->pc) {
-  //   printf("input pc %x\n", cpu.pc);
-  //   printf("ref pc %x\n", ref_r->pc);
-  //   printf("wrong in pc\n");
-  //   return false;
-  // }
+  if (cpu.pc != ref_r->pc) {
+    printf("input pc %x\n", cpu.pc);
+    printf("ref pc %x\n", ref_r->pc);
+    printf("wrong in pc\n");
+    return false;
+  }
   for (int index=0; index < 8; index++) {
     if (cpu.gpr[index]._32 != ref_r->gpr[index]._32) {
       printf("wrong in gpr[%d]\n", index);
