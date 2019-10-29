e0b821c3cc20c7c029763d89321f40ed5a0ba4f8
tracer-ics2019 <tracer@njuics.org>
2019-10-29 10:10:48 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:10:48 up 9 min,  1 user,  load average: 1.42, 1.01, 0.55 87321fda923601886a1bca8d8c2e8b39bb6035bb

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -1,10 +1,11 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
-  printf("add values are %x, %x----------------", id_dest->val, id_src->val);
+  printf("add values are %x, %x----------------\n", id_dest->val, id_src->val);
   rtl_add(&s0, &id_dest->val, &id_src->val);
   rtl_is_add_carry(&s1, &s0, &id_dest->val);
   operand_write(id_dest, &s0);
+  printf("add result is %x", s0);
 
   rtl_update_ZFSF(&s0, id_dest->width);
 
