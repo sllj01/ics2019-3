ac9b763909a4a1d6afbc3d8cba387b20bc9e161c
tracer-ics2019 <tracer@njuics.org>
2019-10-29 20:45:37 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:45:37 up  1:47,  1 user,  load average: 0.41, 0.81, 0.75 775dc80d3e6c3db16df352898916b99721e45a36

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -4,7 +4,7 @@
 make_EHelper(test) {
   //TODO();
   rtl_and(&s0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &s0);
+
   printf("dest is %x, src is %x--------------------\n", id_dest->val, id_src->val);
 
   rtl_li(&ir, 0);
