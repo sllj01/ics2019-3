8b587856c9a00f097bec2a5f95ae05a66794da0f
tracer-ics2019 <tracer@njuics.org>
2019-10-29 19:56:13 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:56:13 up 58 min,  1 user,  load average: 0.94, 0.81, 0.75 6c45fdbafebf2d0865f3a0c83bfaabd94556f52

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -41,7 +41,7 @@ make_group(gp4,
 
 /* 0xff */
 make_group(gp5,
-    EMPTY, EMPTY, EMPTY, EMPTY,
+    EX(inc), EX(dec), EX(call), EMPTY,
     EMPTY, EMPTY, EX(push), EMPTY)//INC, DEC, CALL, CALL, JMP, JMP, PUSH
 
 /* 0x0f 0x01*/
