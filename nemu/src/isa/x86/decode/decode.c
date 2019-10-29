e86b1e29a503bce680d8781f6bb7b6b427d08c9c
tracer-ics2019 <tracer@njuics.org>
2019-10-29 18:03:47 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:03:47 up  4:00,  1 user,  load average: 0.66, 0.95, 0.95 feed0394359f9bf7e76adb119719864ebbe0d3a8

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -311,5 +311,5 @@ make_DHelper(out_a2dx) {
 void operand_write(Operand *op, rtlreg_t* src) {
   if (op->type == OP_TYPE_REG) { rtl_sr(op->reg, src, op->width); }
   else if (op->type == OP_TYPE_MEM) { rtl_sm(&op->addr, src, op->width); }
-  else { printf("-----------pc is %x", cpu.pc); assert(0); }
+  else {assert(0?0:0); }
 }
