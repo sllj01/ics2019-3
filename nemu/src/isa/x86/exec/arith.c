79b8941b455e75de817382fb989507ba0147ec17
tracer-ics2019 <tracer@njuics.org>
2019-10-29 20:38:20 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:38:20 up  1:40,  1 user,  load average: 1.46, 0.71, 0.63 36840e97f3f728c329652d5332b8f6eed53b92e

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -40,12 +40,12 @@ make_EHelper(sub) {
 
 make_EHelper(cmp) {
   //TODO();
-  // rtl_sext(&id_src->val, &id_src->val, id_src->width);
-  rtl_sub(&s0, &id_dest->val, &id_src->val);
+  rtl_sext(&s2, &id_src->val, id_src->width);
+  rtl_sub(&s0, &id_dest->val, &s2);
   printf("----------------------dest and src values are %x, %x\n", id_dest->val, id_src->val);
-  rtl_is_sub_carry(&s1, &s0, &id_dest->val);
+  rtl_is_sub_carry(&s1, &s0, &s2);
   rtl_set_CF(&s1);
-  rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &s2, id_dest->width);
   rtl_set_OF(&s1);
   rtl_update_ZFSF(&s0, id_dest->width);
 
