0977b9d30772a5e360c9f23581b40688e3156f65
tracer-ics2019 <tracer@njuics.org>
2019-11-23 17:00:45 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:00:45 up  1:08,  1 user,  load average: 1.10, 1.08, 0.93 3d0272559e2a65636ff0ba32e79ce206ee8c620

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,17 +1,32 @@
 #include "common.h"
 #include "syscall.h"
 
-extern void _exit(int);
+uint32_t sys_exit(_Context* c) {
+  _halt(c->GPR2);
+  return c->GPRx;
+}
+
+uint32_t sys_yield(_Context* c) {
+  _yield();
+  return 0;
+}
+
+
+
+
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case SYS_exit: _halt(c->GPR2); break;
-    case SYS_yield: _yield(); c->GPRx = 0; break;
+    case SYS_exit: c->GPRx = sys_exit(c); break;     //////////which parameter should I pass to halt???   Conf
+    case SYS_yield: c->GPRx = sys_yield(c); break;
+    // case SYS_write: c->GPRx = sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return c;
 }
+
+
