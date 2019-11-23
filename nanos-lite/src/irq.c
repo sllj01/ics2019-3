1e151046ed5107ba9f1b4b5acb7ab1c369ae9bc4
tracer-ics2019 <tracer@njuics.org>
2019-11-23 17:22:05 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:22:05 up  1:29,  1 user,  load average: 0.87, 0.90, 0.93 8045679219cd957e1ca65aac30e30a29b6484cc0

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,8 +2,8 @@
 extern _Context* do_syscall(_Context*);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: printf("trigger event yield\n"); break;
-    case _EVENT_SYSCALL: printf("trigger event syscall\n"); do_syscall(c); break;
+    case _EVENT_YIELD: /*printf("trigger event yield\n");*/ break;
+    case _EVENT_SYSCALL: /*printf("trigger event syscall\n");*/ do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
