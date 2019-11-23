b351ce903a18763ccbf9226d87a2436091b6189c
tracer-ics2019 <tracer@njuics.org>
2019-11-23 16:27:26 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:27:26 up 35 min,  1 user,  load average: 0.78, 0.97, 0.76 e946946df38c262dff553a8b3c93b03a6ac503f9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,14 @@
 #include "common.h"
 #include "syscall.h"
 
+extern void _exit(int);
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_exit: _halt(c->GPR2); break;
     case SYS_yield: _yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
