30aa45da754040e4b742239b009e72c612c7f54a
tracer-ics2019 <tracer@njuics.org>
2019-11-23 17:20:16 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:20:16 up  1:28,  1 user,  load average: 1.11, 0.97, 0.96 40c1a13e8a42e01fec291243a635919cb6e4a995

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -15,7 +15,6 @@ uint32_t sys_write(_Context* c) {
   int fd = c->GPR2;
   char* buf = (char*) c->GPR3;
   size_t count = (size_t) c->GPR4;
-  printf("--------------%d", count);
 
   if (fd==1||fd==2) {
     for (int index=0; index<count; index++) _putc(*(buf+index));
@@ -32,7 +31,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_exit: Log("    sys_exit\n"); c->GPRx = sys_exit(c); break;     //////////which parameter should I pass to halt???   Conf
     case SYS_yield: Log("   sys_yield\n"); c->GPRx = sys_yield(c); break;
-    case SYS_write: Log("   sys_write\n"); c->GPRx = sys_write(c); break;
+    case SYS_write: /*Log("   sys_write\n")*/; c->GPRx = sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
