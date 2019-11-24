d38a08b0140031be0e93ee06b80e584be2c63b70
tracer-ics2019 <tracer@njuics.org>
2019-11-24 11:13:22 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:13:22 up  1:19,  1 user,  load average: 1.00, 0.94, 0.96 233a0f24a4ba6670867db2ba9f220fff5a384d3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -38,7 +38,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     uint32_t entry_filesize = phdr[index].p_filesz;
     uint32_t entry_memsize = phdr[index].p_memsz;
     uint32_t entry_vaddr = phdr[index].p_vaddr;
-    Log("entry_offset=%x, entry_filesize=%x, entry_memsize=%x, entry_vaddr=%x, type=%x\n------------------\n", entry_offset, entry_filesize, entry_memsize, entry_vaddr, pt_load);
+    // Log("entry_offset=%x, entry_filesize=%x, entry_memsize=%x, entry_vaddr=%x, type=%x\n------------------\n", entry_offset, entry_filesize, entry_memsize, entry_vaddr, pt_load);
     assert(entry_filesize<50000);
 
     ramdisk_read(buf, entry_offset, entry_filesize);
