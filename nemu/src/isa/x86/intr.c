9d713ba791a034c05a9c30c19dba908702cb976e
tracer-ics2019 <tracer@njuics.org>
2019-11-24 20:15:14 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:15:14 up  3:51,  1 user,  load average: 0.99, 0.81, 0.74 c0aa16b9e26b6f8c1573bf487241c11cb021876e

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -13,14 +13,14 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   // uint64_t describe = ((uint64_t)vaddr_read(base+NO*8, 4)<<32) | vaddr_read(base+NO*8+4, 4);
   // uint32_t jmp_des = (describe>>48)<<16 | (describe & 0xFFFF);
   uint32_t jmp_des = (vaddr_read(base+NO*8, 4)&(0x0000ffff)) | (vaddr_read(base+NO*8+4, 4)&(0xffff0000));
-  printf("jmp des is %x\n", jmp_des);
-  printf("test part\n");
-  NO=0x81;
-  jmp_des = (vaddr_read(base+NO*8, 4)&(0x0000ffff)) | (vaddr_read(base+NO*8+4, 4)&(0xffff0000));
-  printf("jmp des for 0x81 is %x\n", jmp_des);
-  NO=0x80;
-  jmp_des = (vaddr_read(base+NO*8, 4)&(0x0000ffff)) | (vaddr_read(base+NO*8+4, 4)&(0xffff0000));
-  printf("jmp des for 0x80 is %x\n", jmp_des);
+  // printf("jmp des is %x\n", jmp_des);
+  // printf("test part\n");
+  // NO=0x81;
+  // jmp_des = (vaddr_read(base+NO*8, 4)&(0x0000ffff)) | (vaddr_read(base+NO*8+4, 4)&(0xffff0000));
+  // printf("jmp des for 0x81 is %x\n", jmp_des);
+  // NO=0x80;
+  // jmp_des = (vaddr_read(base+NO*8, 4)&(0x0000ffff)) | (vaddr_read(base+NO*8+4, 4)&(0xffff0000));
+  // printf("jmp des for 0x80 is %x\n", jmp_des);
   rtl_j(jmp_des);
 }
 
