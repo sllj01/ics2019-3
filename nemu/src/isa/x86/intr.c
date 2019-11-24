9da2f7f8a7aee3a997a69a30411e46baf8e73191
tracer-ics2019 <tracer@njuics.org>
2019-11-24 17:16:55 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:16:55 up 53 min,  1 user,  load average: 0.64, 0.64, 0.56 a2802687aec844e49bdc2a082ac382d3dc85a758

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -12,7 +12,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   uint32_t base = IDTR.base;
   // uint64_t describe = ((uint64_t)vaddr_read(base+NO*8, 4)<<32) | vaddr_read(base+NO*8+4, 4);
   // uint32_t jmp_des = (describe>>48)<<16 | (describe & 0xFFFF);
-  uint32_t jmp_des = vaddr_read(base+NO*8, 2) | (vaddr_read(base+NO*8+6, 2)<<16);
+  uint32_t jmp_des = (vaddr_read(base+NO*8, 4)&(0x0000ffff)) | (vaddr_read(base+NO*8+4, 4)&(0xffff0000));
   printf("jmp des is %x\n", jmp_des);
   rtl_j(jmp_des);
 }
