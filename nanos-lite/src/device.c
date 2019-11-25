676811955c70f0f1a6fd34d1e3b478d648b9e633
tracer-ics2019 <tracer@njuics.org>
2019-11-25 19:00:37 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:00:37 up 30 min,  1 user,  load average: 1.06, 0.96, 0.67 5f9713211620f69d82e517b9a7af7eb288de199

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,7 +2,8 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  for (int index=0; index<len; index++) _putc(*((char*)buf+index));
+  return len;
 }
 
 #define NAME(key) \
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,6 +3,7 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 typedef size_t off_t;
+extern size_t serial_write(const void*, size_t, size_t);
 
 typedef struct {
   char *name;
@@ -28,8 +29,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, invalid_read, invalid_write},
+  {"stdout", 0, 0, invalid_read, serial_write},
+  {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -53,21 +54,22 @@ size_t fs_read(int fd, void* buf, size_t len) {
 }
 
 size_t fs_write(int fd, void* buf, size_t len) {
-  if (fd==1||fd==2) {
-    for (int index=0; index<len; index++) _putc(*((char*)buf+index));
+  if (file_table[fd].write!=NULL) {
+    file_table[fd].write(buf, 0, len);
     return len;
-  }
-
-  size_t rest = file_table[fd].size-file_table[fd].open_offset;
-  if (rest < len) {
-    ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, rest);
-    file_table[fd].open_offset += rest;
-    return rest;
   }
   else {
-    ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
-    file_table[fd].open_offset += len;
-    return len;
+    size_t rest = file_table[fd].size-file_table[fd].open_offset;
+    if (rest < len) {
+      ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, rest);
+      file_table[fd].open_offset += rest;
+      return rest;
+    }
+    else {
+      ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
+      file_table[fd].open_offset += len;
+      return len;
+    }
   }
 }
 
