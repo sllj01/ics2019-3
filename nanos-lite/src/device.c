613589f19432246622d25addb57505457aa8a78c
tracer-ics2019 <tracer@njuics.org>
2019-11-26 20:42:49 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:42:49 up  4:30,  1 user,  load average: 1.21, 1.14, 1.17 ad92539150e8eb66767c3193a9e6ea89d766f60a

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -41,7 +41,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  // assert(offset+len < strlen(dispinfo));
+  assert(offset+len < strlen(dispinfo));
   memcpy(buf, (void*) dispinfo+offset, len);
   return len;
 }
