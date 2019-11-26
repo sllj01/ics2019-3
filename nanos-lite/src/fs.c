70bbae158b9f37f024b8c203284c9f2b1281de95
tracer-ics2019 <tracer@njuics.org>
2019-11-26 21:08:22 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:08:22 up  4:56,  1 user,  load average: 1.38, 1.18, 1.11 732a9a815a570e8e8ac729c612ba48bd9663a35

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -77,7 +77,7 @@ size_t fs_read(int fd, void* buf, size_t len) {
 }
 
 size_t fs_write(int fd, void* buf, size_t len) {
-  if (fd!=1&&fd!=2&&file_table[fd].size-file_table[fd].open_offset < len) {
+  if (fd!=1 && fd!=2 && file_table[fd].size-file_table[fd].open_offset < len) {
     len = file_table[fd].size-file_table[fd].open_offset;
   }
 
