50c113eb364ee2724c625cbe83212adcceb85b66
tracer-ics2019 <tracer@njuics.org>
2019-11-26 20:00:42 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:00:42 up  3:48,  1 user,  load average: 1.53, 2.22, 2.54 b992bcf8daa42fc561bb6b7e6f94fd795c3ca6d

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -68,6 +68,6 @@ void init_device() {
   int width = screen_width();
   int height = screen_height();
   int dlim = sprintf(dispinfo, "WIDTH:%d\n", width);
-  sprintf(dispinfo+dlim, "HEIGHT:%d", height);
+  sprintf(dispinfo+dlim+1, "HEIGHT:%d", height);
   printf("%s\n", dispinfo);
 }
