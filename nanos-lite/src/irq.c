5bef031630e5d05c3f932161b7ace47a42adc003
181220014-Gao Chenxiao <1910342119@qq.com>
2019-12-01 18:09:01 +0800

    finish pa4

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,10 +3,10 @@ extern _Context* do_syscall(_Context*);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD: 
-      Log("yield\n");
+      Log("trigger event yield\n");
       break;
     case _EVENT_SYSCALL: 
-      // printf("trigger event syscall\n"); 
+      Log("trigger event syscall\n"); 
       do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
