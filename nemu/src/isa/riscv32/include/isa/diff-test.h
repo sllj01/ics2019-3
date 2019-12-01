b6b2a23176c979877d923e81405e0c0d8261031c
tracer-ics2019 <tracer@njuics.org>
2019-12-01 17:51:33 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:51:33 up  3:39,  1 user,  load average: 0.53, 0.51, 0.42 3e183c8f67efda3f24b1a49327b6bb6b43a8ae0b

--- nemu/src/isa/riscv32/include/isa/diff-test.h
+++ nemu/src/isa/riscv32/include/isa/diff-test.h
@@ -1,6 +1,6 @@
 #ifndef __RISCV32_DIFF_TEST_H__
 #define __RISCV32_DIFF_TEST_H__
 
-#define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 33) // GRPs + pc
+#define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 34) // GRPs + pc
 
 #endif
