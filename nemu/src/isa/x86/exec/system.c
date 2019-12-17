fc33f7f11ce71023e31c0207bcf5b10ba2892393
tracer-ics2019 <tracer@njuics.org>
2019-12-17 16:23:32 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:23:32 up  1:32,  1 user,  load average: 0.49, 0.66, 0.70 277b02358fcdcbb29778215ae3673b5dde11feaa

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -13,14 +13,26 @@ make_EHelper(lidt) {
 
 make_EHelper(mov_r2cr) {
   //TODO();
-  operand_write(id_dest, &id_src->val);
+  if (id_dest->reg==0) {
+    cpu.CR0 = id_src->val;
+  }
+  else if (id_dest->reg==3) {
+    cpu.CR3 = id_src->val;
+  }
+  else panic("reg not exist!\n");
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
   //TODO();
-  operand_write(id_dest, &id_src->val);
+  if (id_src->reg==0) {
+    operand_write(id_dest, &cpu.CR0);
+  }
+  else if (id_src->reg==3) {
+    operand_write(id_dest, &cpu.CR3);
+  }
+  else panic("reg not exist!\n");
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
