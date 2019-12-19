7b655793cbb956f7eb6b97d2b50ecf8fecc3756a
tracer-ics2019 <tracer@njuics.org>
2019-12-19 16:57:10 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:57:10 up 49 min,  1 user,  load average: 0.54, 0.58, 0.43 6390e2285ff90be038a2e63730207f6ddca9f281

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -3,8 +3,10 @@
 paddr_t page_translate(vaddr_t vaddr) {
   uint32_t CR3 = cpu.CR3;
   uint32_t PG_TBL = paddr_read(CR3+(vaddr>>22), 4);
+  printf("%x\n", PG_TBL);
   if ((PG_TBL&0b1) != 1) assert(0);
   uint32_t PHY_ADDRESS = paddr_read(PG_TBL+(vaddr<<10>>22), 4);
+  printf("%x\n", PHY_ADDRESS);
   if ((PHY_ADDRESS&0b1) != 1) assert(0);
   return PHY_ADDRESS + (vaddr&0xFFF);
 
