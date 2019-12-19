3cae241ef3f70d2676acce5b23f7a5ade8b23f54
tracer-ics2019 <tracer@njuics.org>
2019-12-19 16:54:32 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:54:32 up 46 min,  1 user,  load average: 0.58, 0.72, 0.44 d58ae1dfe5c9356c3e4c48617fb78acd2116915

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,9 +1,27 @@
 #include "nemu.h"
 
+paddr_t page_translate(vaddr_t vaddr) {
+  uint32_t CR3 = cpu.CR3;
+  uint32_t PG_TBL = paddr_read(CR3+(vaddr>>22), 4);
+  if ((PG_TBL&0b1) != 1) assert(0);
+  uint32_t PHY_ADDRESS = paddr_read(PG_TBL+(vaddr<<10>>22), 4);
+  if ((PHY_ADDRESS&0b1) != 1) assert(0);
+  return PHY_ADDRESS + (vaddr&0xFFF);
+
+}
+
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if ((addr>>12)!=((addr+len)>>12)) assert(0);
+  else {
+    paddr_t paddr = page_translate(addr);
+    return paddr_read(paddr, len);
+  }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+  if ((addr>>12)!=((addr+len)>>12)) assert(0);
+  else {
+    paddr_t paddr = page_translate(addr);
+    paddr_write(paddr, data, len);
+  }
 }
