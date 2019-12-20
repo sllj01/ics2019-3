9a34c9f196baf03882e86bef19064793b15fd54e
tracer-ics2019 <tracer@njuics.org>
2019-12-20 14:28:23 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:28:23 up 27 min,  1 user,  load average: 1.38, 1.28, 0.95 b032bee6c9c46921294ff78f3e4424a3dc2c2ec

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -176,6 +176,7 @@ void context_kload(PCB *pcb, void *entry) {
 void context_uload(PCB *pcb, const char *filename) {
   _protect(&pcb->as);
   uintptr_t entry = loader(pcb, filename);
+  printf("loader returns %d\n", entry);
 
   _Area stack;
   stack.start = pcb->stack;
