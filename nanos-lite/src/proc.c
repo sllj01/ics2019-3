1eda2c380a505ec09707ca36e917e4fa87e2bbd7
tracer-ics2019 <tracer@njuics.org>
2019-12-20 18:18:53 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:18:53 up 24 min,  1 user,  load average: 1.13, 1.01, 0.70 3c3f755a83bcde8f82ba8f3d9750c62bd4fd937a

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   // context_kload(&pcb[0], (void*)hello_fun);
-  context_uload(&pcb[0], "/bin/init");
+  context_uload(&pcb[0], "/bin/text");
   switch_boot_pcb();
 
   Log("Initializing processes...");
