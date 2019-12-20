f832eedc62a92c75ae5eb4565502fb394ae431c7
tracer-ics2019 <tracer@njuics.org>
2019-12-20 12:55:30 +0800

    >  run 181220014 derek Linux do-not-connect-this 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  12:55:30 up 17 min,  1 user,  load average: 1.69, 0.88, 0.51 baa7acf460966b00a090d8c8d7562acabb52486e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -106,6 +106,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         fs_read(fd, (void*) buf, 4096);
         va = phdr[index].p_vaddr+phdr[index].p_filesz-left;
         pa = (paddr_t) new_page(1);
+        Log("set map va=%p, pa=%p\n", va, pa);
         _map(&pcb->as, (void*)va, (void*)pa, 1);
         memcpy((void*)pa, buf, 4096);
         left-=4096;
