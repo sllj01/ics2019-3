db08afefc97b3dc738eda2937a8da7d4786df213
tracer-ics2019 <tracer@njuics.org>
2019-12-21 10:18:45 +0800

    >  compile 181220014 derek Linux do-not-connect-this 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:18:45 up 16:24,  1 user,  load average: 0.65, 0.65, 0.71 d97e057ebe3341a150932a86e720f841b162cab4

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -29,11 +29,11 @@ int mm_brk(uintptr_t brk, intptr_t increment) {
   if (new_brk>current->max_brk) {
     printf("-------------pre_max_brk=%d, inc=%d, current_brk=%d\n", current->max_brk, increment, brk);
     int left = new_brk-current->max_brk;
-    int res = (4096-(current->max_brk%4096))%4096;
+    int res = (4096-(current->max_brk%4096)-1)%4096;
     left = left-res;
         printf("left=%d\n", left);
 
-    void* va = (void*) (((current->max_brk-1)/4096+1)<<12);
+    void* va = (void*) ((current->max_brk/4096+1)<<12);
     while(left > 0) {
       void* pa = new_page(1);
       printf("in mm_brk, va=%d, pa=%d\n", va, pa);
